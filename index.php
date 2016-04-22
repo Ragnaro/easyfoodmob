575a48a24b400f264d9d7b57bdef9e13cc73eafe
Pedro.Angel <arcangel251049@gmail.com>
2016-04-22 06:17:41 -0500

    Update like 2
    Update Botton

--- index.php
+++ index.php
@@ -255,7 +255,7 @@
                 </a>
             </div>
             <!-- fin codigo www.gratisparaweb.com -->
-            <div class="fb-like" data-href="https://www.facebook.com/Easyfood-1688031498114750/" data-width="700px" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+            <div class="fb-like" data-href="https://www.facebook.com/Easyfood-1688031498114750/" data-width="100px" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
 
 
         </div>
